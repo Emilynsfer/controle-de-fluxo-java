22a6a7c7cbe16bb7882750e5530af6b52be2f2eb
Emilyn Fernandes <143656727+Emilynsfer@users.noreply.github.com>
2024-12-04 20:55:11 -0300

    Update Contador.java

--- src/Contador.java
+++ src/Contador.java
@@ -17,13 +17,13 @@ public class Contador {
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		//validando se parametroUm é MAIOR que parametroDois e lançando a exceção
 		if (parametroUm > parametroDois) {
 			throw new ParametrosInvalidosException();
 		}
 		
 		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
+		//realizando o for para imprimir os números com base na variável contagem
 		for (int i = 0; i < contagem; i++) {
 			System.out.println(i);
 		}
